7e50f58cc9f038c0dae353fe286eb3303f147424
ShadowInSpace <ivan.yackovec@gmail.com>
2022-10-14 23:39:52 +0300

    finished linkenList

--- src/main/java/MyLinkedList.java
+++ src/main/java/MyLinkedList.java
@@ -1,7 +1,7 @@
 import java.util.LinkedList;
 
 public class MyLinkedList {
-    private int size = 0;
+    private int size;
     private static class Node {
         Object item;
         MyLinkedList.Node next;
@@ -16,6 +16,10 @@ public class MyLinkedList {
     private Node first;
     private Node last;
 
+    public int size(){
+        return size;
+    }
+
     void add(Object o) {
         final MyLinkedList.Node l = last;
         final MyLinkedList.Node newNode = new MyLinkedList.Node(l, o, null);
@@ -27,23 +31,59 @@ public class MyLinkedList {
         size++;
     }
     public MyLinkedList() {
-    size=0;
-    }
-    public void remove(int index){
-    MyLinkedList.Node next;
-    MyLinkedList.Node prev = first;
-    if(index>=size) throw new IllegalArgumentException();
-
-    for(int i=0; i<index; i++){
-    next = prev.next;
-
+        size=0;
     }
 
-    }
+    public void remove(int index) {
+        final MyLinkedList.Node x = getNode(index);
+        final MyLinkedList.Node prev = x.prev;
+        final MyLinkedList.Node next = x.next;
 
+        if (prev == null) {
+            //if x first element;
+            first = next;
+            next.prev = null;
+        } else if (next == null) {
+            //if x last element
+            last = x.prev;
+            last.next = null;
+        } else {
+            //x not first nor last
+            prev.next = next;
+            next.prev = prev;
+        }
+        size--;
+        x.next = x.prev = null;
+
+    }
     public Object get(int index){
-        MyLinkedList.Node next;
-        MyLinkedList.Node prev = first;
+        return getNode(index).item;
+    }
+
+    public void clear(){
+        for (MyLinkedList.Node x = first; x != null; ) {
+            MyLinkedList.Node next = x.next;
+            x.item = null;
+            x.next = null;
+            x.prev = null;
+            x = next;
+        }
+        first = last = null;
+        size = 0;
+    }
+
+    private MyLinkedList.Node getNode(int index){
+        if(index>=size) throw new IllegalArgumentException();
+        MyLinkedList.Node nodeA = first;
+        MyLinkedList.Node nodeB;
+
+        for(int i=0; i<index; i++){
+            nodeB = nodeA.next;
+            nodeA = nodeB;
+        }
+        return nodeA;
+    }
 
     }
-}
+
+
